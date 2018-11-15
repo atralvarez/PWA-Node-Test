dc79290760de4fb89a2f22ceff3b12ed7537d56e
Aitor Alvarez <atr.alvarez@gmail.com>
2018-11-15 18:47:40 +0100

    update

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ app.get("/public/dist/manifest.json", function(req, res){
   });
 
 
-  app.listen(app.get('port'), ()=>{
+  app.listen(3000, ()=>{
     console.log("server running @ localhost:5000");
 });
